bb1e50037e87b9df721c83bd9b9dbdf574fcf757
Thomas Gossart <tg.59290@gmail.com>
2017-06-25 17:49:28 +0200

    changed the js script to calculate the currente position of the mousse and the element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-$("#main").append("<div class='pixel'></div>");
+var mousseLeft = 0;
+var mousseTop = 0;
+var objectPosition = $("#main").position();
+var objectLeftPosition = objectPosition.left;
+var objectTopPosition = objectPosition.top;
+
+$( document ).on( "mousemove", function( event ) {
+  mousseLeft = event.pageX;
+  mousseTop = event.pageY;
+});
